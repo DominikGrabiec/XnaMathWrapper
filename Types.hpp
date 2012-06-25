365e7f6d23fb0a71ef9d2f4d1d86bde786eae387
Dominik Grabiec <dominik@dominikgrabiec.com>
2012-06-25 23:33:27 +1000

    Add Visual Studio 2010 project and related files.

---
+++ Types.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef __TYPES_HPP__
+#define __TYPES_HPP__
+
+typedef unsigned char uchar_t;
+typedef unsigned short ushort_t;
+typedef unsigned int uint_t;
+typedef unsigned long ulong_t;
+
+typedef signed char schar_t;
+typedef signed short sshort_t;
+typedef signed int sint_t;
+typedef signed long slong_t;
+
+#include <cstdint>
+
+#endif // __TYPES_HPP__
---
+++ Windows.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef __WINDOWS_HPP__
+#define __WINDOWS_HPP__
+
+#define STRICT
+
+// Require Windows Vista and above
+#define WINVER         _WIN32_WINNT_VISTA
+#define _WIN32_WINDOWS _WIN32_WINNT_VISTA
+#define _WIN32_WINNT   _WIN32_WINNT_VISTA
+
+#define NOMINMAX
+
+#include <Windows.h>
+
+#endif // __WINDOWS_HPP__
